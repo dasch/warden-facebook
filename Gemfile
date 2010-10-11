0bc5909c47e2fe251aeaa04a78fbaaf3204242a3
Daniel Schierbeck <daniel.schierbeck@gmail.com>
2010-10-11 09:33:25 +0200

    Add test and dev dependencies

--- Gemfile
+++ Gemfile
@@ -1,8 +1,16 @@
+source 'http://rubygems.org/'
+
 gem 'warden'
 gem 'oauth2'
 gem 'json'
 
+group :development do
+  gem 'shotgun'
+end
+
 group :test do
   gem 'shoulda'
+  gem 'webrat'
+  gem 'rack-test', :require => 'rack/test'
   gem 'sinatra', '~>1.0', :require => 'sinatra/base'
 end
--- test/helper.rb
+++ test/helper.rb
@@ -1,11 +1,26 @@
 require 'rubygems'
+require 'bundler'
 require 'test/unit'
-require 'shoulda'
+
+Bundler.require(:default, :test)
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 
 require 'warden/facebook'
+require 'app'
+
+Webrat.configure do |config|
+  config.mode = :rack
+  config.application_port = 4567
+end
 
 class Test::Unit::TestCase
+  include Rack::Test::Methods
+  include Webrat::Methods
+  include Webrat::Matchers
+
+  def app
+    Example.app
+  end
 end
